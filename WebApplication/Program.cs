33d1e7664be1dd32f62eb16b23db146bc7994b54
Андрей Демьянов <demyanovaf@vsk.sibur.ru>
2021-12-11 23:42:27 +0300

    Рефакторинг

--- WebApplication/Program.cs
+++ WebApplication/Program.cs
@@ -1,8 +1,10 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
+
+//builder.Configuration.AddCommandLine(args);
+
 var app = builder.Build();
 
 //var configuration = app.Configuration;
-
 //var greetings = configuration["CustomGreetings"];
 
 app.MapGet("/", () => app.Configuration["CustomGreetings"]);
