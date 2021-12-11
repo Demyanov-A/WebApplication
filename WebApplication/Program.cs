998767dea7349d66ef2a24d9be9bc28b3169e384
Андрей Демьянов <demyanovaf@vsk.sibur.ru>
2021-12-12 00:31:48 +0300

    Добавление главного контроллера

--- WebApplication/Program.cs
+++ WebApplication/Program.cs
@@ -1,12 +1,29 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿var builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(args);
 
 //builder.Configuration.AddCommandLine(args);
 
+var services = builder.Services;
+
+services.AddControllersWithViews();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseRouting();
 
 //var configuration = app.Configuration;
 //var greetings = configuration["CustomGreetings"];
+//app.MapGet("/", () => app.Configuration["CustomGreetings"]);
 
-app.MapGet("/", () => app.Configuration["CustomGreetings"]);
+app.MapGet("/throw", () =>
+{
+    throw new  ApplicationException("Ошибка в программе!");
+});
+
+app.MapDefaultControllerRoute();
 
 app.Run();
