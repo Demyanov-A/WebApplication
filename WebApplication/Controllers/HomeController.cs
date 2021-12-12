a30e46e9de76e882bb4e02d294083a2da631592b
Андрей Демьянов <demyanovaf@vsk.sibur.ru>
2021-12-12 14:33:08 +0300

    Страничка со списком сотрудников

--- WebApplication/Controllers/HomeController.cs
+++ WebApplication/Controllers/HomeController.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication.Models;
 
 namespace WebApplication.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly List<Employee> __Employees = new()
+        {
+            new Employee() { Id = 1, FirstName = "Иван", LastName = "Иванов", Patronymic = "Иванович", Age = 21},
+            new Employee() { Id = 2, FirstName = "Петр", LastName = "Петров", Patronymic = "Петрович", Age = 22 },
+            new Employee() { Id = 3, FirstName = "Сидор", LastName = "Сидоров", Patronymic = "Сидорович", Age = 23 },
+        };
         public IActionResult Index()
         {
             //return Content("Данные из первого контроллера!");
@@ -13,6 +20,11 @@ namespace WebApplication.Controllers
         public string ConfiguredAction(string id, string Value1)
         {
             return $"Hello World {id} - {Value1}";
+        }
+
+        public IActionResult Employees()
+        {
+            return View(__Employees);
         }
     }
 }
