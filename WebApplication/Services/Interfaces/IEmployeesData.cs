a03c8225e33756a7bd11b3648eccb73e4cd2604e
Андрей Демьянов <demyanovaf@vsk.sibur.ru>
2021-12-19 00:21:41 +0300

    Интерфейс сервиса управления сотрудниками

---
+++ WebApplication/Services/Interfaces/IEmployeesData.cs
@@ -0,0 +1,17 @@
+﻿using WebApplication.Models;
+
+namespace WebApplication.Services.Interfaces
+{
+    public interface IEmployeesData
+    {
+        IEnumerable<Employee> GetAll();
+
+        Employee? GetById(int id);
+
+        bool Edit(Employee employee);
+
+        bool Delete(int id);
+
+        bool Add(Employee employee);
+    }
+}
