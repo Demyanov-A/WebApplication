647eade2fb052a08869b3e22dc38595f45cc4bc2
Андрей Демьянов <demyanovaf@vsk.sibur.ru>
2021-12-24 01:26:05 +0300

    Каталог товаров

---
+++ WebApplication/Controllers/CatalogController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication.Domain;
+using WebApplication.Services.Interfaces;
+using WebApplication.ViewModels;
+
+namespace WebApplication.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private readonly IProductData _ProductData;
+
+        public CatalogController(IProductData ProductData) => _ProductData = ProductData;
+
+        public IActionResult Index(int? BrandId, int? SectionId)
+        {
+            var filter = new ProductFilter
+            {
+                BrandId = BrandId,
+                SectionId = SectionId,
+            };
+
+            var products = _ProductData.GetProducts(filter);
+
+            var catalog_model = new CatalogViewModel
+            {
+                BrandId = BrandId,
+                SectionId = SectionId,
+                Products = products
+                    .OrderBy(p => p.Order)
+                    .Select(p => new ProductViewModel
+                    {
+                        Id = p.Id,
+                        Name = p.Name,
+                        Price = p.Price,
+                        ImageURL = p.ImageURL,
+                    }),
+            };
+
+            return View(catalog_model);
+        }
+    }
+}
