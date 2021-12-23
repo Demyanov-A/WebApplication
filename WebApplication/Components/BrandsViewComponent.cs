7a8eacf075866eb43f9f1a4e54f7e6ee9b71e612
Андрей Демьянов <demyanovaf@vsk.sibur.ru>
2021-12-23 23:00:26 +0300

    Модель-педставления секции

---
+++ WebApplication/Components/BrandsViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Components
+{
+    public class BrandsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication/Components/SectionsViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MessagePack.Resolvers;
+using WebApplication.Services.Interfaces;
+
+namespace WebApplication.Components
+{
+    public class SectionsViewComponent : ViewComponent
+    {
+        private readonly IProductData _ProductData;
+        public SectionsViewComponent(IProductData ProductData) => _ProductData = ProductData;
+
+        public IViewComponentResult Invoke()
+        {
+            var sections = _ProductData.GetSections();
+
+            return View();
+        }
+    }
+}
