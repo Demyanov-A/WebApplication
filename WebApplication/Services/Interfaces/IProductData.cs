3cc9bd2cdbe42dc7a586823424ce2998e81c4b1b
Андрей Демьянов <demyanovaf@vsk.sibur.ru>
2021-12-23 22:31:51 +0300

    Мнтерфейс сервиса каталога товаров

---
+++ WebApplication/Services/Interfaces/IProductData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication.Domain.Entities.Base;
+
+namespace WebApplication.Services.Interfaces
+{
+    public interface IProductData
+    {
+        IEnumerable<Section> GetSections();
+
+        IEnumerable<Brand> GetBrands();
+    }
+}
