73ae489f00afa964955320d87ddf30d12f0e5b1d
Андрей Демьянов <demyanovaf@vsk.sibur.ru>
2021-12-23 23:57:40 +0300

    товар

---
+++ WebApplication.Domain/Entities/Base/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication.Domain.Entities.Base.Interfaces;
+
+namespace WebApplication.Domain.Entities.Base
+{
+    public class Product : NamedEntity, IOrderEntity
+    {
+        public int Order { get; set; }
+        public int SectionId { get; set; }
+        public int? BrandId { get; set; }
+        public string ImageURL { get; set; }
+        public decimal Price { get; set; }
+    }
+}
