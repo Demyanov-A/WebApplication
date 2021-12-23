3f418abcd4797fe66d7843e834bbeb8f8f9aa0a6
Андрей Демьянов <demyanovaf@vsk.sibur.ru>
2021-12-23 23:15:38 +0300

    Логика визуального компонента формирования списка секций

--- WebApplication/Components/SectionsViewComponent.cs
+++ WebApplication/Components/SectionsViewComponent.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using MessagePack.Resolvers;
 using WebApplication.Services.Interfaces;
+using WebApplication.ViewModels;
 
 namespace WebApplication.Components
 {
@@ -18,7 +19,36 @@ namespace WebApplication.Components
         {
             var sections = _ProductData.GetSections();
 
-            return View();
+            var parent_sections = sections.Where(s => s.ParentId is null);
+            var parent_sections_views = parent_sections
+                .Select(s => new SectionViewModel
+                {
+                    Id = s.Id,
+                    Name = s.Name,
+                    Order = s.Order,
+                })
+                .ToList();
+            foreach (var parent_section in parent_sections_views)
+            {
+                var childs = sections.Where(s => s.ParentId == parent_section.Id);
+
+                foreach (var child_section in childs)
+                {
+                    parent_section.ChildSections.Add(new SectionViewModel
+                    {
+                        Id = child_section.Id,
+                        Name = child_section.Name,
+                        Order = child_section.Order,
+                        Parent = parent_section
+                    });
+                }
+
+                parent_section.ChildSections.Sort((a, b) => Comparer<int>.Default.Compare(a.Order, b.Order));
+            }
+
+            parent_sections_views.Sort((a,b) => Comparer<int>.Default.Compare(a.Order,b.Order));
+            
+            return View(parent_sections_views);
         }
     }
 }
