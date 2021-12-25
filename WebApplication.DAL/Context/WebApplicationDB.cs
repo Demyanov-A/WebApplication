450a136856c413c5d43ca89661a7fcc64fce6a32
Андрей Демьянов <demyanovaf@vsk.sibur.ru>
2021-12-25 22:55:35 +0300

    Определение контекста БД

---
+++ WebApplication.DAL/Context/WebApplicationDB.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication.Domain.Entities;
+
+namespace WebApplication.DAL.Context
+{
+    public class WebApplicationDB : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Section> Sections { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public WebApplicationDB(DbContextOptions<WebApplicationDB> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder db)
+        {
+            base.OnModelCreating(db);
+
+            //db.Entity<Section>()
+            //    .HasMany(section => section.Products)
+            //    .WithOne(product => product.Section)
+            //    .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
