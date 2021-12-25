6d59d5eb64a11f431d96faa92ebb69ed46467f40
Андрей Демьянов <demyanovaf@vsk.sibur.ru>
2021-12-25 22:42:05 +0300

    Подготовка модели данных для размещения в БД

--- WebApplication.Domain/Entities/Product.cs
+++ WebApplication.Domain/Entities/Product.cs
@@ -1,19 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using WebApplication.Domain.Entities.Base;
 using WebApplication.Domain.Entities.Base.Interfaces;
 
 namespace WebApplication.Domain.Entities
 {
+    [Index(nameof(Name))]
     public class Product : NamedEntity, IOrderEntity
     {
         public int Order { get; set; }
         public int SectionId { get; set; }
+
+        [ForeignKey((nameof(SectionId)))]
+        public Section Section { get; set; }
         public int? BrandId { get; set; }
+
+        [ForeignKey((nameof(BrandId)))]
+        public Brand Brand { get; set; }
         public string ImageURL { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
     }
 }
