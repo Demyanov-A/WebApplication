8b113052bb161381f7610f81945bb7b224878fd0
Андрей Демьянов <demyanovaf@vsk.sibur.ru>
2021-12-26 22:23:07 +0300

    Реализация сервиса каталога в БД

---
+++ WebApplication/Services/InSQL/SqlProductData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication.DAL.Context;
+using WebApplication.Domain;
+using WebApplication.Domain.Entities;
+using WebApplication.Services.Interfaces;
+
+namespace WebApplication.Services.InSQL
+{
+    public class SqlProductData : IProductData
+    {
+        private readonly WebApplicationDB _db;
+
+        public SqlProductData(WebApplicationDB db) => _db = db;
+
+        public IEnumerable<Brand> GetBrands() => _db.Brands;
+
+        public IEnumerable<Product> GetProducts(ProductFilter? Filter = null)
+        {
+            IQueryable<Product> query = _db.Products;
+
+            if (Filter?.SectionId != null)
+                query = query.Where(p => p.SectionId == Filter.SectionId);
+
+            if (Filter?.BrandId != null)
+                query = query.Where(p => p.BrandId == Filter.BrandId);
+
+            return query;
+        }
+
+        public IEnumerable<Section> GetSections() => _db.Sections;
+    }
+}
