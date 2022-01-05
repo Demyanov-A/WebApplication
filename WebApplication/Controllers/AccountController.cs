498ad5306ae2bf9050fa20981305df5f2512b269
Андрей Демьянов <demyanovaf@vsk.sibur.ru>
2022-01-05 23:49:04 +0300

    Визуальная часть контроллера Account

---
+++ WebApplication/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register() => View();
+        public IActionResult Login() => View();
+
+        public IActionResult Logout() => RedirectToAction("Index", "Home");
+        public IActionResult AccessDenied() => View();
+    }
+}
