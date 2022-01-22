b27f6c7fae7806ff0f2b06e504afd9a6df0b89a9
Андрей Демьянов <demyanovaf@vsk.sibur.ru>
2022-01-22 23:10:23 +0300

    Логика контроллера корзины для оформления нового заказа

---
+++ WebApplication/ViewModels/CartOrderViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication.ViewModels
+{
+    public class CartOrderViewModel
+    {
+        public CartViewModel Cart { get; set; } = null!;
+
+        public OrderViewModel Order { get; set; } = new();
+    }
+}
