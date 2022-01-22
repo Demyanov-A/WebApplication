16f6c92ce6373724cd297db4099b186641cb48a3
Андрей Демьянов <demyanovaf@vsk.sibur.ru>
2022-01-22 21:34:22 +0300

    Разметка области администратора

--- WebApplication/Areas/Admin/Controllers/HomeController.cs
+++ WebApplication/Areas/Admin/Controllers/HomeController.cs
@@ -4,10 +4,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using WebApplication.Domain.Entities.Identity;
 
 namespace WebApplication.Areas.Admin.Controllers
 {
-    [Area("Admin")]
+    [Area("Admin"), Authorize(Roles = Role.Administrators)]
     public class HomeController : Controller
     {
         public IActionResult Index() => View();
