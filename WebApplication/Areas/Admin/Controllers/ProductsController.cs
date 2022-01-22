8a7cb5732a60635c123f8c2bfce607cd04e286b8
Андрей Демьянов <demyanovaf@vsk.sibur.ru>
2022-01-22 22:13:06 +0300

    Редактор товаров

--- WebApplication/Areas/Admin/Controllers/ProductsController.cs
+++ WebApplication/Areas/Admin/Controllers/ProductsController.cs
@@ -3,12 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication.Domain.Entities.Identity;
+using WebApplication.Services.Interfaces;
 
 namespace WebApplication.Areas.Admin.Controllers
 {
+    [Area("Admin"), Authorize(Roles = Role.Administrators)]
     public class ProductsController : Controller
     {
-        public IActionResult Index() => View();
+        private readonly IProductData _ProductData;
+        private readonly ILogger<ProductsController> _Logger;
+
+        public ProductsController(IProductData ProductData, ILogger<ProductsController> Logger)
+        {
+            _ProductData = ProductData;
+            _Logger = Logger;
+        }
+
+        public IActionResult Index()
+        {
+            var products = _ProductData.GetProducts();
+            return View(products);
+        }
     }
 }
