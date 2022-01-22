c63f9b1583e8221a2122d149263195353f0aaa92
Андрей Демьянов <demyanovaf@vsk.sibur.ru>
2022-01-22 23:23:35 +0300

    Профиль пользователя с отображением в нём списка заказов

--- WebApplication/Controllers/UserProfileController.cs
+++ WebApplication/Controllers/UserProfileController.cs
@@ -4,11 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication.Services.Interfaces;
+using WebApplication.ViewModels;
 
 namespace WebApplication.Controllers
 {
     public class UserProfileController : Controller
     {
         public IActionResult Index() => View();
+        public async Task<IActionResult> Orders([FromServices] IOrderService OrderService)
+        {
+            var orders = await OrderService.GetUserOrdersAsync(User.Identity!.Name!);
+
+            return View(orders.Select(order => new UserOrderViewModel
+            {
+                Id = order.Id,
+                Address = order.Address,
+                Phone = order.Phone,
+                Description = order.Description,
+                TotalPrice = order.TotalPrice,
+                Date = order.Date,
+            }));
+        }
     }
 }
