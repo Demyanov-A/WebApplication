32bc83e3c1462b8500c68c97dccae27d341ee10b
Андрей Демьянов <demyanovaf@vsk.sibur.ru>
2022-01-24 00:30:31 +0300

    Регистрация сервисов бизнес-логики в проекте хоста WebAPI

--- WebApplication.WebAPI/Program.cs
+++ WebApplication.WebAPI/Program.cs
@@ -1,3 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+using WebApplication.DAL.Context;
+using WebApplication.Domain.Entities.Identity;
+using WebApplication.Interfaces.Services;
+using WebApplication.Services.Services;
+using WebApplication.Services.Services.InSQL;
+
 var builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +17,53 @@ services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 services.AddEndpointsApiExplorer();
 services.AddSwaggerGen();
+
+var database_type = builder.Configuration["Database"];
+switch (database_type)
+{
+    default: throw new InvalidOperationException($"Тип БД {database_type} не поддерживается");
+
+    case "SqlServer":
+        services.AddDbContext<WebApplicationDB>(opt =>
+            opt.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
+        break;
+
+    case "Sqlite":
+        services.AddDbContext<WebApplicationDB>(opt =>
+            opt.UseSqlite(builder.Configuration.GetConnectionString("Sqlite"),
+                o => o.MigrationsAssembly("WebStore.DAL.Sqlite")));
+        break;
+}
+
+services.AddTransient<IDbInitializer, DbInitializer>();
+
+services.AddIdentity<User, Role>()
+    .AddEntityFrameworkStores<WebApplicationDB>()
+    .AddDefaultTokenProviders();
+
+services.Configure<IdentityOptions>(opt =>
+{
+#if DEBUG
+    opt.Password.RequireDigit = false;
+    opt.Password.RequireLowercase = false;
+    opt.Password.RequireUppercase = false;
+    opt.Password.RequireNonAlphanumeric = false;
+    opt.Password.RequiredLength = 3;
+    opt.Password.RequiredUniqueChars = 3;
+#endif
+
+    opt.User.RequireUniqueEmail = false;
+    opt.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIGKLMNOPQRSTUVWXYZ1234567890";
+
+    opt.Lockout.AllowedForNewUsers = false;
+    opt.Lockout.MaxFailedAccessAttempts = 10;
+    opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
+});
+
+services.AddScoped<IEmployeesData, SqlEmployeesData>();
+services.AddScoped<IProductData, SqlProductData>();
+services.AddScoped<IOrderService, SqlOrderService>();
+//services.AddScoped<ICartService, InCookiesCartService>();
 
 var app = builder.Build();
 
