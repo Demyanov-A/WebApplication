78fa06baa0dcce36c0d83fd967f46dd51f1ecf64
Андрей Демьянов <demyanovaf@vsk.sibur.ru>
2022-01-23 14:02:14 +0300

    Визуальное управление клиентом тестового WebAPI

---
+++ UI/WebApplication/Controllers/WebAPIController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication.Interfaces.TestAPI;
+
+namespace WebApplication.Controllers
+{
+    public class WebAPIController : Controller
+    {
+        private readonly IValuesService _ValuesService;
+
+        public WebAPIController(IValuesService ValuesService) => _ValuesService = ValuesService;
+
+        public IActionResult Index()
+        {
+            var values = _ValuesService.GetValues();
+            return View(values);
+        }
+    }
+}
