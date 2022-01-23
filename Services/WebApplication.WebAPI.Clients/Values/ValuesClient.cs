7b3e173d871aea91d102d2d6c5fc33e3e94578dd
Андрей Демьянов <demyanovaf@vsk.sibur.ru>
2022-01-23 13:46:06 +0300

    Заготовка клиента тестового WebAPI

---
+++ Services/WebApplication.WebAPI.Clients/Values/ValuesClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication.Interfaces.TestAPI;
+using WebApplication.WebAPI.Clients.Base;
+
+namespace WebApplication.WebAPI.Clients.Values
+{
+    public class ValuesClient : BaseClient, IValuesService
+    {
+        public ValuesClient(HttpClient Client) : base(Client, "api/values")
+        {
+
+        }
+
+        public IEnumerable<string> GetValues() { throw new NotImplementedException(); }
+
+        public int Count() { throw new NotImplementedException(); }
+
+        public string GetById(int Id) { throw new NotImplementedException(); }
+
+        public void Add(string Value) { throw new NotImplementedException(); }
+
+        public void Edit(int Id, string Value) { throw new NotImplementedException(); }
+
+        public bool Delete(int Id) { throw new NotImplementedException(); }
+    }
+}
