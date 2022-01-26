73f6af60245a14f760266de03648a409773cd085
Андрей Демьянов <demyanovaf@vsk.sibur.ru>
2022-01-26 22:36:16 +0300

    Интерфейсы, которые требуеют реализации

---
+++ WebApplication.Interfaces/Services/Identity/IRolesClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using WebApplication.Domain.Entities.Identity;
+
+namespace WebApplication.Interfaces.Services.Identity
+{
+    public interface IRolesClient : IRoleStore<Role>
+    {
+
+    }
+}
---
+++ WebApplication.Interfaces/Services/Identity/IUsersClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using WebApplication.Domain.Entities.Identity;
+
+namespace WebApplication.Interfaces.Services.Identity
+{
+    public interface IUsersClient :
+        IUserRoleStore<User>,
+        IUserPasswordStore<User>,
+        IUserEmailStore<User>,
+        IUserPhoneNumberStore<User>,
+        IUserTwoFactorStore<User>,
+        IUserLoginStore<User>,
+        IUserClaimStore<User>
+    {
+
+    }
+}
