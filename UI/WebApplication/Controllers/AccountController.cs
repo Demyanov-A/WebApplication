911b0e012760915667df37439247af36e86d03a0
Андрей Демьянов <demyanovaf@vsk.sibur.ru>
2022-02-01 23:05:11 +0300

    Добавление логирования в AccountController

--- UI/WebApplication/Controllers/AccountController.cs
+++ UI/WebApplication/Controllers/AccountController.cs
@@ -15,11 +15,16 @@ namespace WebApplication.Controllers
     {
         private readonly UserManager<User> _UserManager;
         private readonly SignInManager<User> _SignInManager;
+        private readonly ILogger<AccountController> _Logger;
 
-        public AccountController(UserManager<User> UserManager, SignInManager<User> SignInManager)
+        public AccountController(
+            UserManager<User> UserManager,
+            SignInManager<User> SignInManager,
+            ILogger<AccountController> Logger)
         {
             _UserManager = UserManager;
             _SignInManager = SignInManager;
+            _Logger = Logger;
         }
 
         public IActionResult Register() => View(new RegisterUserViewModel());
@@ -30,27 +35,40 @@ namespace WebApplication.Controllers
             if (!ModelState.IsValid)
                 return View(Model);
 
-            //IEnumerable<RegisterUserViewModel> models = Enumerable.Repeat(Model, 5);
-            //var users = Mapper.Map<IEnumerable<User>>(models);
+            //var str0 = $"Hello, {Model.UserName}";
+            //var str1 = string.Format("Hello, {0}", Model.UserName);
+            //var str2 = string.Concat("Hello, ", Model.UserName);
 
-            var user = Mapper.Map<User>(Model);
+            //_Logger.LogInformation($"Начало процедуры регистрации пользователя {Model.UserName}"); // неправильно
+            //_Logger.LogInformation("Начало процедуры регистрации пользователя {0}", Model.UserName);
+            _Logger.LogInformation("Начало процедуры регистрации пользователя {UserName}", Model.UserName);
 
-            //var user = new User
-            //{
-            //    UserName = Model.UserName,
-            //};
-
-            var registration_result = await _UserManager.CreateAsync(user, Model.Password).ConfigureAwait(true);
-            if (registration_result.Succeeded)
+            using (_Logger.BeginScope("Регистрация {UserName}", Model.UserName))
             {
-                await _UserManager.AddToRoleAsync(user, Role.Users).ConfigureAwait(true);
+                var user = Mapper.Map<User>(Model);
 
-                await _SignInManager.SignInAsync(user, false).ConfigureAwait(true);
-                return RedirectToAction("Index", "Home");
+                var registration_result = await _UserManager.CreateAsync(user, Model.Password).ConfigureAwait(true);
+                if (registration_result.Succeeded)
+                {
+                    _Logger.LogInformation("Пользователь {0} зарегистрирован", user.UserName);
+
+                    await _UserManager.AddToRoleAsync(user, Role.Users).ConfigureAwait(true);
+
+                    _Logger.LogInformation("Пользователь {0} наделён ролью {1}", user.UserName, Role.Users);
+
+                    await _SignInManager.SignInAsync(user, false).ConfigureAwait(true);
+
+                    _Logger.LogInformation("Пользователь {0} временно вошёл в систему после регистрации", user.UserName);
+
+                    return RedirectToAction("Index", "Home");
+                }
+
+                var errors = string.Join(",", registration_result.Errors.Select(e => e.Description));
+                _Logger.LogWarning("При регистрации {0} возникли ошибки:{1}", user.UserName, errors);
+
+                foreach (var error in registration_result.Errors)
+                    ModelState.AddModelError("", error.Description);
             }
-
-            foreach (var error in registration_result.Errors)
-                ModelState.AddModelError("",error.Description);
 
             return View(Model);
 
@@ -64,6 +82,8 @@ namespace WebApplication.Controllers
             if (!ModelState.IsValid)
                 return View(Model);
 
+            _Logger.LogInformation("Попытка входа в систему {0}", Model.UserName);
+
             var login_result = await _SignInManager.PasswordSignInAsync(
                 Model.UserName,
                 Model.Password,
@@ -72,6 +92,8 @@ namespace WebApplication.Controllers
 
             if (login_result.Succeeded)
             {
+                _Logger.LogInformation("Пользователь {0} успешно вошёл в систему", Model.UserName);
+
                 //if (Url.IsLocalUrl(Model.ReturnUrl))
                 //    return Redirect(Model.ReturnUrl);
                 //return RedirectToAction("Index", "Home");
@@ -80,16 +102,25 @@ namespace WebApplication.Controllers
 
             }
 
+            _Logger.LogWarning("Ошибка входа пользователя {0}", Model.UserName);
+
             ModelState.AddModelError("","Неверное имя пользователя или пароль! ");
             return View(Model);
         }
 
         public async Task<IActionResult> Logout()
         {
+            var user_name = User.Identity!.Name;
             await _SignInManager.SignOutAsync().ConfigureAwait(true);
+            _Logger.LogInformation("Пользователь {0} вышел из системы", user_name);
             return RedirectToAction("Index", "Home");
         }
 
-        public IActionResult AccessDenied() => View();
+        public IActionResult AccessDenied()
+        {
+            _Logger.LogWarning("Ошибка доступа к {0}", ControllerContext.HttpContext.Request.Path);
+            return View();
+        }
+
     }
 }
