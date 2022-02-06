e40695f5dd430dc07f88da423f938c083a70bb17
Андрей Демьянов <demyanovaf@vsk.sibur.ru>
2022-02-06 22:36:01 +0300

    Первый модульный тест

---
+++ Tests/WebApplication.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication.Controllers;
+using Assert = Xunit.Assert;
+
+
+namespace WebApplication.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void ConfiguredAction_Returns_string_value()
+        {
+            const string id = "123";
+            const string value_1 = "QWE";
+            const string expected_string = $"Hello World! {id} - {value_1}";
+
+            var controller = new HomeController();
+
+            var actual_string = controller.ConfiguredAction(id, value_1);
+
+            Assert.Equal(expected_string, actual_string);
+        }
+    }
+}
