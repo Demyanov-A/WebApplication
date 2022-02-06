957e1811df7a9fbe3e45de0e802e16e25b092c85
Андрей Демьянов <demyanovaf@vsk.sibur.ru>
2022-02-06 22:39:05 +0300

    Пример структуры модульных тестов

--- Tests/WebApplication.Tests/Controllers/HomeControllerTests.cs
+++ Tests/WebApplication.Tests/Controllers/HomeControllerTests.cs
@@ -16,15 +16,28 @@ namespace WebApplication.Tests.Controllers
         [TestMethod]
         public void ConfiguredAction_Returns_string_value()
         {
+
+            #region Arrange
+
             const string id = "123";
             const string value_1 = "QWE";
             const string expected_string = $"Hello World! {id} - {value_1}";
 
             var controller = new HomeController();
 
+            #endregion
+
+            #region Act
+
             var actual_string = controller.ConfiguredAction(id, value_1);
 
+            #endregion
+
+            #region Assert
+
             Assert.Equal(expected_string, actual_string);
+
+            #endregion
         }
     }
 }
