6bfd96b84fa0b3110028599a3b3aa47549a6b529
Андрей Демьянов <demyanovaf@vsk.sibur.ru>
2022-02-06 23:18:16 +0300

    Выделение отдельного сервиса хранения данных корзины

---
+++ Services/WebApplication.Interfaces/Services/ICartStore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication.Domain.Entities;
+
+namespace WebApplication.Interfaces.Services
+{
+    public interface ICartStore
+    {
+        public Cart Cart { get; set; }
+    }
+}
