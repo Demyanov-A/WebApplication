257f486f60b8e3f973cd3b41ea2d55c87864f32b
Андрей Демьянов <demyanovaf@vsk.sibur.ru>
2022-02-14 00:12:11 +0300

    Подсветка активной секции и активного бренда в боковой навигационной панели

---
+++ Common/WebApplication.Domain/ViewModels/SelectableSectionsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication.Domain.ViewModels
+{
+    public class SelectableSectionsViewModel : IEnumerable
+    {
+        public IEnumerable<SectionViewModel> Sections { get; set; }
+
+        public int? SectionId { get; set; }
+
+        public int? ParentSectionId { get; set; }
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
