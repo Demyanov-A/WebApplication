8bbaedc073e1e7678ccf179d56e4ed8946579c02
Андрей Демьянов <demyanovaf@vsk.sibur.ru>
2022-02-14 23:36:11 +0300

    Отладка логики скрипта корзины - добавление товара

--- UI/WebApplication/Controllers/CartController.cs
+++ UI/WebApplication/Controllers/CartController.cs
@@ -70,7 +70,7 @@ namespace WebApplication.Controllers
         public IActionResult DecrementAPI(int Id)
         {
             _CartService.Decrement(Id);
-            return Ok();
+            return Ok(new { Id, message = $"Товар {Id} был убавлен на 1" });
         }
 
         public IActionResult RemoveAPI(int Id)
